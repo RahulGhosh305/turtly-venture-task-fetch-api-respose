88edb0bbed1d1ae0f541b5fd3772224fc9551f45
Rahul Ghosh <rahulghosh096@gmail.com>
2022-05-30 00:50:12 +0600

    Tast 2 Fetch API Response JS GET POST DELETE

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+const token = "d7c01847de4c083cb154e9a533294301e9f05f93dbae7d589e42ece63226c0a3"
+
 // GET DATA
 const fetchData = () => {
     fetch("https://gorest.co.in/public/v1/users").then(response => {
@@ -13,6 +15,8 @@ const fetchData = () => {
                 <p>Name: ${user.name}</p>
                 <p>Email: ${user.email}</p>
                 <p>Name: ${user.gender}</p>
+
+                <button class="btn btn-primary" onclick='deleteOne(${user.id})'>Delete</button>
             </div>
             `
         }).join("")
@@ -26,23 +30,82 @@ const fetchData = () => {
 fetchData()
 
 
-let form = document.getElementById('form')
-console.log(form)
+// Get from FORM DATA
+let formData = {}
+let PostForm = document.getElementById('postForm')
+PostForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+        ;[...this.elements].forEach(el => {
+            if (el.type !== 'submit') {
+                formData[el.name] = el.value
+            }
+        })
+    // console.log(formData)
+    // CALL POST function
+    postData(formData.email, formData.name, formData.gender, formData.status)
+})
 
 
-// POST DATA
-// const token = "d7c01847de4c083cb154e9a533294301e9f05f93dbae7d589e42ece63226c0a3"
-// const postData = () => {
-//     console.log(name, email, gender, status)
+// POST DATA fetch
+const postData = (email, name, gender, status) => {
+    console.log("Data", email, name, gender, status)
+    fetch("https://gorest.co.in/public/v1/users", {
+        method: 'POST',
+        body: JSON.stringify({
+            email: email,
+            name: name,
+            gender: gender,
+            status: status
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Authorization': `Bearer ${token}`
+        }
+    }).then(response => {
+        if (!response.ok) {
+            throw Error("Error Found")
+        }
+        return response.json()
+    }).then(data => {
+        // Success
+        return alert("Created")
+    }).catch(error => {
+        console.log(error)
+    })
 
-//     fetch("https://gorest.co.in/public/v1/users", {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             email,
-//             gender,
-//             name,
-//             status
-//         }),
+}
+
+
+
+
+
+// DELETE Data fetch
+const deleteOne = (id) => {
+    console.log("delete", id)
+    fetch(`https://gorest.co.in/public/v1/users/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Authorization': `Bearer ${token}`
+        }
+    }).then(response => {
+        if (!response.ok) {
+            throw Error("Error Found")
+        }
+    }).then(data => {
+        // Success
+        return alert("Deleted")
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+
+// Update Fetch
+// const update = (id) => {
+//     console.log("update", id)
+//     fetch(`https://gorest.co.in/public/v1/users/${id}`, {
+//         method: 'PATCH',
 //         headers: {
 //             'Content-type': 'application/json; charset=UTF-8',
 //             'Authorization': `Bearer ${token}`
@@ -51,12 +114,12 @@ console.log(form)
 //         if (!response.ok) {
 //             throw Error("Error Found")
 //         }
-//         return response.json()
 //     }).then(data => {
-//         return alert("Created")
+//         // Success
+//         return alert("Deleted")
 //     }).catch(error => {
 //         console.log(error)
 //     })
-
 // }
-// postData()
+
+
